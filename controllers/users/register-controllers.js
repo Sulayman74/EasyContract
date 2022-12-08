1d7a3aead3ba940c54b98d13b79dbea99d3bf16a
MacMini de Sulayman <macmini@Mac-mini-de-MacMini.local>
2022-12-08 14:01:29 +0100

    nouveaux controllers

--- controllers/users/register-controllers.js
+++ controllers/users/register-controllers.js
@@ -1,20 +1,17 @@
-//TODO pouvoir ajouter des salariés coté entreprise et/ou une entreprise sans passer par le register features au cas où... //
+// ** register a worker */
 
 const pool = require("../../config");
 const bcrypt = require("bcrypt")
 const validator = require("validator")
 const { isEmail } = validator;
 
-
-//** Add a Worker */
-
-exports.addWorker = async (req, res) => {
+exports.registerWorker = async (req, res) => {
 
     //! Je vérifie si mon salarié existe par son email qui est unique */
 
     const { email } = req.body
 
-    let worker = await pool.query("SELECT email FROM salarie WHERE email=$1", [email])
+    let worker = await pool.query("SELECT email FROM salarie WHERE email=$1", [email]);
 
     //** Je verifie le format de l'email via validator et isEmail */
     if (!isEmail(email)) {
@@ -41,14 +38,11 @@ exports.addWorker = async (req, res) => {
             let hash = await bcrypt.hash(mdp, saltRounds);
             mdp = hash
 
-            const addWorker = await pool.query(
+            const registerWorker = await pool.query(
                 "INSERT INTO salarie (civilite,nom,prenom,telephone,rue,cp,ville,email,mdp,role,nom_jeune_fille,num_ss, date_naissance, lieu_naissance,pays_naissance) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15) RETURNING *",
                 [civilite, nom, prenom, telephone, rue, cp, ville, email, mdp, role, nom_jeune_fille, num_ss, date_naissance, lieu_naissance, pays_naissance]
             );
-
-
-            res.status(200).json({ "addAWorker": addWorker.rows[0] })
-
+            res.status(200).json({ "registerAWorker": registerWorker.rows[0] })
 
         } catch (error) {
             console.error(error.message);
@@ -60,22 +54,24 @@ exports.addWorker = async (req, res) => {
 }
 
 
-//** Add a Society */
 
-exports.addSociety = async (req, res) => {
+//! ---------------------------------------------------- */
+
+//** Register a Society */
+
+exports.registerSociety = async (req, res) => {
 
     //! Je vérifie si mon entreprise existe par son email,siret qui est unique */
 
     const { siret, email } = req.body
-    console.log(siret, email);
     let society = await pool.query("SELECT siret,email FROM entreprise WHERE siret=$1 AND email=$2", [siret, email])
+
     //** Je verifie le format de l'email via validator et isEmail */
     if (!isEmail(email)) {
         console.log("invalid email");
         res.status(402).json({ "email": email, "message": "invalid email" })
         return false
     }
-
 
     if (society.rowCount !== 0) {
         console.log("Can not add this society");
@@ -87,22 +83,22 @@ exports.addSociety = async (req, res) => {
 
     try {
 
-        // console.log(req.body);
+
         const { civilite, nom, prenom, telephone, rue, cp, ville, email, role, siret, raison_sociale, code_ape } = req.body;
 
         //** ---- hash du mot de passe via bcrypt */
-
-        let { mdp } = req.body;
         const saltRounds = 12;
+        let { mdp } = req.body;
         let hash = await bcrypt.hash(mdp, saltRounds);
         mdp = hash
+
 
         const addSociety = await pool.query(
             "INSERT INTO entreprise (civilite,nom,prenom,telephone,rue,cp,ville,email,mdp,role,siret,raison_sociale,code_ape) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13) RETURNING *",
             [civilite, nom, prenom, telephone, rue, cp, ville, email, mdp, role, siret, raison_sociale, code_ape]
         );
         res.status(200).json({ "addASociety": addSociety.rows[0] })
-        console.log('A Society has been added correctly', req.body);
+        console.log('A Society has been added correctly');
 
     } catch (error) {
         console.error(error.message);
--- routes/users-router.js
+++ routes/users-router.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const userRouter = express.Router();
 
-//** ajout de users */
+//** ajout de users/register/login */
 const addWorker = require("../controllers/users/add-controller");
+const registerWorker = require("../controllers/users/register-controllers");
 const addSociety = require("../controllers/users/add-controller");
+const registerSociety = require("../controllers/users/register-controllers")
 //** recherche de tous les users ou d'un seul en particulier par son id */
 const getAllUsers= require("../controllers/users/get-all-controller");
-const getWorker = require("../controllers/users/get-one-controller");
-const getSociety = require("../controllers/users/get-one-controller");
+const getaWorker = require("../controllers/users/get-one-controller");
+const getaSociety = require("../controllers/users/get-one-controller");
+const getWorkers = require("../controllers/users/get-all-controller");
+const getSocieties = require("../controllers/users/get-all-controller");
 
 //** suppression d'un salarié ou entreprise ( suppression compte) */
 
@@ -17,22 +21,38 @@ const deleteSociety = require("../controllers/users/delete-controller");
 //** mise à jour des données */
 const updateWorker = require("../controllers/users/update-controller")
 const updateSociety = require("../controllers/users/update-controller")
-//** hash mdp */
-const hashPassword = require("../controllers/users/login-register")
 
+// ! ------------------------------------------------ */
+
+//** All the users route */
 userRouter.get("/allUsers", getAllUsers.getAllUsers);
-userRouter.get("/oneWorker/:id",  getWorker.getWorker)
-userRouter.get("/oneSociety/:id",  getSociety.getSociety)
 
+//** Users by Id  */
+userRouter.get("/oneWorker/:id",  getaWorker.getWorker);
+userRouter.get("/oneSociety/:id",  getaSociety.getSociety);
+
+//** All the workers */
+userRouter.get("/workers", getWorkers.getAllWorkers);
+
+//** All the societies */
+userRouter.get("/societies", getSocieties.getAllSocieties);
+
+//** worker add register login */
+userRouter.post("/registerWorker", registerWorker.registerWorker);
 userRouter.post("/createWorker", addWorker.addWorker);
+
+//** society add register login */
+userRouter.post("/registerSociety", registerSociety.registerSociety)
 userRouter.post("/createSociety", addSociety.addSociety);
 
+//** delete section */
 userRouter.delete("/deleteWorker/:id", deleteWorker.deleteWorker);
 userRouter.delete("/deleteSociety/:id", deleteSociety.deleteSociety)
 
+//** update section */
 userRouter.put("/updateWorker/:id", updateWorker.updateWorker)
 userRouter.put("/updateSociety/:id", updateSociety.updateSociety)
 
-userRouter.post("/login", )
+// userRouter.post("/login", )
 
 module.exports = userRouter; 
