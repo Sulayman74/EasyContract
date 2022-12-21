bcd78cf1d0bb93e8006529f3a86505e546e86e8b
MacMini de Sulayman <macmini@Mac-mini-de-MacMini.local>
2022-12-21 16:27:10 +0100

    modif pour matcher les id

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,25 +1,32 @@
 
 const jwt = require("jsonwebtoken");
-const {StatusCodes}= require('http-status-codes');
+const { StatusCodes } = require('http-status-codes');
 
 
 // Define a middleware function that uses the verifyToken function to
 // authenticate requests
+
 function authenticateToken(req, res, next) {
-  
-  const authHeader = req.header('authorization'); //Bearer TOKEN
+  const attr = 'authorization'
+
+  const authHeader = req.headers[attr] //Bearer TOKEN
   const token = authHeader && authHeader.split(' ')[1];
 
-  if (token == null) return res.status(StatusCodes.BAD_REQUEST).json({ error: "not allowed null token" });
+  console.log(token);
+  if (!token) return res.status(StatusCodes.BAD_REQUEST).json({ error: "not allowed null token" });
 
 
   try {
+
     const decoded = jwt.verify(token, process.env.ACCESS_TOKEN);
-    req.user = decoded;
-} catch (err) {
-    return res.status(StatusCodes.FORBIDDEN).send("Unauthorized");
-}
-return next();
+    req.entreprise = decoded;
+    req.salarie = decoded;
+
+  } catch (err) {
+
+    return res.status(StatusCodes.FORBIDDEN).json({ "catch": "Unauthorized", err: message });
+  }
+  return next();
 }
 
 module.exports = authenticateToken
