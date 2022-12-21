ab0ee79ec95e32fd7d53b96688cfa3507efbf056
MacMini de Sulayman <macmini@Mac-mini-de-MacMini.local>
2022-12-21 12:33:18 +0100

    modif auth tokens

--- helpers/auth_helper.js
+++ helpers/auth_helper.js
@@ -3,9 +3,9 @@ const jwt = require("jsonwebtoken")
 
 
 
-function jwtTokens({ email, utilisateur_id, role }) {
+function jwtTokens({ email, utilisateur_id, role, mdp }) {
 
-    const user = { email, utilisateur_id, role };
+    const user = { email, utilisateur_id, role, mdp };
     const accessToken = jwt.sign(user,process.env.ACCESS_TOKEN)
     return accessToken
 }
