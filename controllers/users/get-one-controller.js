0214253e382d090af511079a0cbcc87b2be2a65b
MacMini de Sulayman <macmini@Mac-mini-de-MacMini.local>
2022-12-22 17:39:14 +0100

    modif controllers

--- controllers/users/get-one-controller.js
+++ controllers/users/get-one-controller.js
@@ -5,12 +5,13 @@ const pool = require("../../config")
 exports.getWorker = async (req, res) => {
 
     const  {utilisateur_id}  = req.salarie
-    console.log(utilisateur_id);
     try {
         
         const getWorker = await pool.query(
             "SELECT * FROM salarie WHERE salarie_id=$1", [utilisateur_id]
         );
+        if (getWorker.rows.length === 0) return res.status(400).json({ error: "no worker found" });
+
         res.status(200).json({ "message": "You have chosen this worker", "worker": getWorker })
 
     } catch (error) {
@@ -24,7 +25,7 @@ exports.getSociety = async (req, res) => {
 
         const { utilisateur_id } = req.entreprise
         const getSociety = await pool.query(
-            "SELECT * FROM entreprise WHERE entreprise_id=$1", [utilisateur_id]
+            "SELECT * FROM entreprise WHERE entreprise_id=$1",[utilisateur_id]
         );
         if (getSociety.rows.length === 0) return res.status(400).json({ error: "no society found" });
         
