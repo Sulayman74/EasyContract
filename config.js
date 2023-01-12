753f57a3f705c59417cb755c5acec32b88aa3e96
MacMini de Sulayman <macmini@Mac-mini-de-MacMini.local>
2023-01-12 11:07:47 +0100

    maj config.js

--- config.js
+++ config.js
@@ -1,14 +1,24 @@
 // const Pool = require("pg").Pool;
-const {Pool} = require("pg");
+const { Pool } = require("pg");
 require("dotenv").config();
 
 
+
 const pool = new Pool({
-  user: process.env.USER,
+  user: process.env.BDD_USER,
   password: process.env.PASSWORD,
   host: process.env.HOST,
-  port: process.env.PORT_CONFIG,
+  port: 5432,
   database: process.env.DATABASE,
+  ssl : true
 });
+// const pool = new Pool({
+//   user: process.env.USER,
+//   password: process.env.PASSWORD,
+//   host: process.env.HOST,
+//   port: process.env.PORT_CONFIG,
+//   database: process.env.DATABASE,
+//   ssl: true
+// });
 
 module.exports = pool;
