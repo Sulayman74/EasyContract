bbc49e54ea08b88e1488dfbb8fb97056af718361
MacMini de Sulayman <macmini@mac-mini-de-macmini.home>
2023-02-17 11:58:18 +0100

    commentaires ajoutées

--- config.js
+++ config.js
@@ -1,4 +1,7 @@
-// const Pool = require("pg").Pool;
+// ** autre façon d'écrire destructuring
+// TODO const Pool = require("pg").Pool;
+// ** autre façon d'écrire destructuring **/
+
 const { Pool } = require("pg");
 require("dotenv").config();
 
@@ -13,13 +16,14 @@ const pool = new Pool({
   ssl: true
 });
 
+// ? pour le localhost
 // const pool = new Pool({
 //   user: process.env.USER,
 //   password: process.env.PASSWORD,
 //   host: process.env.HOST,
 //   port: process.env.PORT_CONFIG,
 //   database: process.env.DATABASE,
-
 // });
+// ? pour le localhost
 
 module.exports = pool;
