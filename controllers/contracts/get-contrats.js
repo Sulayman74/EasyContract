23f785483de14ff16b8f64140c9bd9781d85a1d3
MacMini de Sulayman <macmini@Mac-mini-de-MacMini.local>
2023-02-27 16:35:03 +0100

    essai get contrat

--- controllers/contracts/get-contrats.js
+++ controllers/contracts/get-contrats.js
@@ -5,6 +5,7 @@ exports.getContrats = async (req, res) => {
     const { utilisateur_id } = req.entreprise
 
     try {
+        
         console.log(utilisateur_id);
         const salarie = await pool.query(
 `
@@ -32,5 +33,61 @@ entreprise.utilisateur_id =$1
     } catch (error) {
 
         res.status(StatusCodes.BAD_REQUEST).json({ "error": error.message, "message": req.entreprise })
+    }
+}
+
+// ! GET all contrat pour une entreprise
+exports.getAllMyContratEntreprise = async (req, res) => {
+    try {
+
+        const { utilisateur_id } = req.entreprise
+        console.log(utilisateur_id);
+        const allContrat = await pool.query(`SELECT
+        salarie.pays_naissance, 
+        salarie.lieu_naissance, 
+        salarie.date_naissance, 
+        salarie.num_ss, 
+        salarie.nom_jeune_fille, 
+        salarie.salarie_id, 
+        salarie.mdp, 
+        salarie.email, 
+        salarie.ville, 
+        salarie.cp, 
+        salarie.rue, 
+        salarie.telephone, 
+        salarie.prenom, 
+        salarie.nom, 
+        salarie.civilite, 
+        salarie.utilisateur_id, 
+        contrat.fki_entreprise, 
+        contrat.contrat_id, 
+        contrat.fki_salarie, 
+        contrat.type_contrat, 
+        contrat.is_fulltime, 
+        contrat.date_debut, 
+        contrat.remuneration, 
+        contrat.statut, 
+        contrat.fonction, 
+        contrat.motif, 
+        contrat.date_fin, 
+        contrat.periode_essai 
+       
+    FROM
+        salarie
+        INNER JOIN
+        contrat
+        ON 
+            contrat.fki_salarie = salarie.salarie_id
+        INNER JOIN
+        entreprise
+        ON 
+            contrat.fki_entreprise = entreprise.entreprise_id
+    WHERE
+        contrat.fki_entreprise = $1`, [utilisateur_id]);
+        console.log(allContrat)
+        res.json(allContrat.rows);
+    } catch (err) {
+        console.error(err.message)
+        res.status(400).send("Server error")
     }
 }
