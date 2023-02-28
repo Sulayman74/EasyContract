3e6565a39b7ddc2b8565b41a491abe95364e9184
MacMini de Sulayman <macmini@Mac-mini-de-MacMini.local>
2023-02-28 14:25:51 +0100

    modif controller

--- controllers/users/get-my-salaries.js
+++ controllers/users/get-my-salaries.js
@@ -31,7 +31,7 @@ FROM
 		contrat.fki_entreprise = entreprise.entreprise_id
         WHERE
         contrat.fki_entreprise = $1`, [utilisateur_id]);
-        res.status(StatusCodes.OK).json({"datas worker": getMyWorkers.rows})
+        res.status(StatusCodes.OK).json({"datasworker": getMyWorkers.rows})
 
 
     } catch (error) {
