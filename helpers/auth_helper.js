4421dcf31560a9fe5e2aec711e67e0cff81c5cee
Sulayman <sirikone@hotmail.com>
2023-03-10 17:43:03 +0100

    todo pour token lui donner un temps d'expiration

--- helpers/auth_helper.js
+++ helpers/auth_helper.js
@@ -6,8 +6,11 @@ const jwt = require("jsonwebtoken")
 function jwtTokens({ email, utilisateur_id, role, mdp }) {
 
     const user = { email, utilisateur_id, role, mdp };
-    const accessToken = jwt.sign(user,process.env.ACCESS_TOKEN)
-    return accessToken
+    const access_Token = jwt.sign(user, process.env.ACCESS_TOKEN)
+
+    // **TODO JE DEVRAIS AJOUTER UN TEMPS D'EXPIRATION POUR LE TOKEN ET PENSER À FAIRE UN REFRESH TOKEN const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN, {expiresIn: "20m"})*/
+
+    return access_Token
 }
-module.exports = {jwtTokens}
+module.exports = { jwtTokens }
 
